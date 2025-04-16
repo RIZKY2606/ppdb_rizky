ee570fe63b6b4e4b25d70228d89557d6d8b828ab
RIZKY2606 <149983460+RIZKY2606@users.noreply.github.com>
2025-04-16 11:38:27 +0700

    vendor

---
+++ lang/en/message.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+'students'=> [
+    'store' => 'Penambahan data :namasiswa berhasil',
+    'update' => 'Pergantian data :namasiswa berhasil',
+    'delete' => 'Penghapusan data :namasiswa berhasil',
+],
+
+'user'=> [
+    'store' => 'Penambahan data :name berhasil',
+    'update' => 'Pergantian data :name berhasil',
+    'delete' => 'Penghapusan data :name berhasil',
+]
+];
